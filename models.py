6e11372cb6f429d9e3ad3b2865e01e002b00ef0e
AGUS DHARMAWAN <96321168+agdhr@users.noreply.github.com>
2025-06-14 11:12:04 +0700

    Add files via upload

--- models.py
+++ models.py
@@ -54,28 +54,18 @@ conv -> BN -> Activation -> conv -> BN -> Activation -> Dropout (if enabled)
 """
 def unet_2d(input_size, n_class=1,):
     inputs = layers.Input(input_size)
-	# 网络结构定义，数据处理的时候已经转化为灰度图了
     conv1 = layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(inputs)
-    #print ("conv1 shape:",conv1.shape)
     conv1 = layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv1)
-    #print ("conv1 shape:",conv1.shape)
     pool1 = layers.MaxPooling2D(pool_size=(2, 2))(conv1)
-    #print ("pool1 shape:",pool1.shape)
-
+    
     conv2 = layers.Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool1)
-    #print ("conv2 shape:",conv2.shape)
     conv2 = layers.Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv2)
-    #print ("conv2 shape:",conv2.shape)
     pool2 = layers.MaxPooling2D(pool_size=(2, 2))(conv2)
-    #print ("pool2 shape:",pool2.shape)
-
+    
     conv3 = layers.Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool2)
-    #print ("conv3 shape:",conv3.shape)
     conv3 = layers.Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv3)
-    #print ("conv3 shape:",conv3.shape)
     pool3 = layers.MaxPooling2D(pool_size=(2, 2))(conv3)
-    #print ("pool3 shape:",pool3.shape)
-
+    
     conv4 = layers.Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(pool3)
     conv4 = layers.Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv4)
     drop4 = layers.Dropout(0.5)(conv4)
@@ -86,32 +76,27 @@ def unet_2d(input_size, n_class=1,):
     drop5 = layers.Dropout(0.5)(conv5)
 
     up6 = layers.Conv2D(512, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(layers.UpSampling2D(size = (2,2))(drop5))
-    # merge6 = merge([drop4,up6], mode = 'concat', concat_axis = 3)
     merge6 = layers.Concatenate(axis=3)([drop4, up6])
     conv6 = layers.Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge6)
     conv6 = layers.Conv2D(512, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv6)
 
     up7 = layers.Conv2D(256, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(layers.UpSampling2D(size = (2,2))(conv6))
-    # merge7 = merge([conv3,up7], mode = 'concat', concat_axis = 3)
     merge7 = layers.Concatenate(axis=3)([conv3, up7])
     conv7 = layers.Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge7)
     conv7 = layers.Conv2D(256, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv7)
 
     up8 = layers.Conv2D(128, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(layers.UpSampling2D(size = (2,2))(conv7))
-    # merge8 = merge([conv2,up8], mode = 'concat', concat_axis = 3)
     merge8 = layers.Concatenate(axis=3)([conv2,up8])
     conv8 = layers.Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge8)
     conv8 = layers.Conv2D(128, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv8)
 
     up9 = layers.Conv2D(64, 2, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(layers.UpSampling2D(size = (2,2))(conv8))
-    # merge9 = merge([conv1,up9], mode = 'concat', concat_axis = 3)
     merge9 = layers.Concatenate(axis=3)([conv1,up9])
     conv9 = layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(merge9)
     conv9 = layers.Conv2D(64, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv9 = layers.Conv2D(2, 3, activation = 'relu', padding = 'same', kernel_initializer = 'he_normal')(conv9)
     conv10 = layers.Conv2D(n_class, 1, activation = 'sigmoid')(conv9)
-	# [batch, 512, 512, 1]
-
+	
     model = models.Model(inputs = inputs, outputs = conv10)
     model.compile(optimizer = optimizers.Adam(learning_rate = 1e-4), loss = 'binary_crossentropy', metrics = [iou, jacard_coef,dice_coef])
     return model
